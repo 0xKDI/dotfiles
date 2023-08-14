a1073d5df7f6ba984114143f62e77efb283660ad
Dmitry Kulikov <dummy@email.com>
2023-08-14 16:39:53 +0300

    up

---
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+  { "Mofiqul/dracula.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "dracula",
+    },
+  },
+  {
+    "nvim-lualine/lualine.nvim",
+    opts = {
+      theme = "dracula-nvim",
+    },
+  },
+}
