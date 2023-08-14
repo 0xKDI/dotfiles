bb08033797600b291e5dd211a27166fed10dff1d
Dmitry Kulikov <dummy@email.com>
2023-08-14 12:45:27 +0300

    up

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -15,7 +15,7 @@ require("lazy").setup({
     -- { import = "lazyvim.plugins.extras.lang.json" },
     -- { import = "lazyvim.plugins.extras.ui.mini-animate" },
     -- import/override with your plugins
-    { import = "lazyvim.plugins.extras.lang.terraform" },
+    -- { import = "lazyvim.plugins.extras.lang.terraform" },
     -- { import = "lazyvim.plugins.extras.lang.docker" },
     { import = "lazyvim.plugins.extras.lang.go" },
     { import = "lazyvim.plugins.extras.lang.json" },
