f4b95fa51128af3fcedeee1abea2434eac33022f
Dmitry Kulikov <dummy@email.com>
2023-08-31 19:30:05 +0300

    up

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,36 +1,11 @@
-if true then
-  return {}
-end
-
 return {
-  {
-    "nvim-treesitter/nvim-treesitter",
-    opts = function(_, opts)
-      if type(opts.ensure_installed) == "table" then
-        vim.list_extend(opts.ensure_installed, {
-          "terraform",
-          "hcl",
-        })
-      end
-    end,
-  },
   {
     "neovim/nvim-lspconfig",
     opts = {
-      servers = {
-        terraformls = {},
+      diagnostics = {
+        underline = false,
+        virtual_text = false,
       },
     },
-  },
-  {
-    "jose-elias-alvarez/null-ls.nvim",
-    opts = function(_, opts)
-      if type(opts.sources) == "table" then
-        local null_ls = require("null-ls")
-        vim.list_extend(opts.sources, {
-          null_ls.builtins.formatting.terraform_fmt,
-        })
-      end
-    end,
   },
 }
